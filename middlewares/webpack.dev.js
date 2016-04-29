526a4f7732a96816822294fb3147a127f98f9b02
unknown <>
2016-04-29 23:45:46 +0800

    Adaptation windows

--- middlewares/webpack.dev.js
+++ middlewares/webpack.dev.js
@@ -2,6 +2,6 @@ import convert from 'koa-convert';
 import proxy from 'koa-proxy';
 
 export default convert(proxy({
-  host: 'http://0.0.0.0:8081/build',
+  host: 'http://127.0.0.1:8081/build',
   match: /^\/build/,
 }));
