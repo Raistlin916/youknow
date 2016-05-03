85805384c99367e15a996734ad730535dce49370
gaokai <gaokai@qima-inc.comgit>
2016-05-03 14:20:39 +0800

    add buildings

--- static/scripts/containers/buildings.js
+++ static/scripts/containers/buildings.js
@@ -1,79 +1,23 @@
 const data = {
   zixun: {
     name: '周边资讯',
-    items: [{
-      id: 1,
-      title: 'test',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 2,
-      title: 'test2',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 3,
-      title: 'test3',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 4,
-      title: 'test4',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 5,
-      title: 'test5',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 6,
-      title: 'test6',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 7,
-      title: 'test7',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 8,
-      title: 'test8',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 9,
-      title: 'test2',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 10,
-      title: 'test2',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 11,
-      title: 'test2',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 12,
-      title: 'test2',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 13,
-      title: 'test2',
-      url: 'http://www.baidu.com'
-    }, {
-      id: 14,
-      title: 'test2',
-      url: 'http://www.baidu.com'
-    }]
+    items: [{"title":"租房资讯 *公司地址","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569593"},{"title":"周边停车资讯","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569596"},{"title":"周边美食资讯","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569598"},{"title":"周末去哪玩","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569600"},{"title":"公交卡&公园卡","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569602"}]
   },
   hr: {
     name: 'HR指引',
-    items: []
+    items: [{"title":"社保","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569659"},{"title":"公积金","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=3768423"},{"title":"户口档案","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=3179583"},{"title":"党组织关系","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569662"},{"title":"期权科普","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569664"},{"title":"试用期总结","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569666"},{"title":"职级说明","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569668"},{"title":"转岗 & 晋升等说明","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569670"},{"title":"绩效白皮书（okr宣讲）","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569673"},{"title":"内部推荐流程和奖金发放","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=2557455"},{"title":"异地面试、异地入职报销","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=2557310"},{"title":"文化白皮书","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569678"},{"title":"公司奖惩约定","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=2557438"},{"title":"公司组织架构图","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569681"},{"title":"工作时间/请假/放假/年假等说明","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=2557434"}]
   },
   enjoy: {
     name: '福利ENJOY',
-    items: []
+    items: [{"title":"餐补-E卡","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569630"},{"title":"新生儿礼包","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569632"},{"title":"团建 & outing","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=4851878"},{"title":"有赞定制私人医生服务","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569635"},{"title":"电脑补贴和设备使用说明","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569637"},{"title":"通讯费 & 交通费补贴","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569640"},{"title":"员工体检","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569643"},{"title":"有赞俱乐部","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=983411"},{"title":"有赞小卖部","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569647"},{"title":"有赞书库","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=983456"},{"title":"有赞币","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569650"},{"title":"内部分享和培训","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569652"},{"title":"一眼“望穿”有赞","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569655"}]
   },
   tiyan: {
     name: '初体验',
-    items: []
+    items: [{"title":"前台电话&开票信息&通讯录","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569606"},{"title":"花名申请","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569608"},{"title":"师兄/姐的神奇功效","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569611"},{"title":"hrbp的360°解读","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569613"},{"title":"有赞的微信群","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569615"},{"title":"新人培训","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569617"},{"title":"新人拓展","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569619"},{"title":"徒步hiking","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569621"},{"title":"新人破冰下午茶","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569624"},{"title":"星座生日会","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569626"},{"title":"节日活动 *集锦","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569628"}]
   },
   zhanghao: {
     name: '账号指南',
-    items: []
+    items: [{"title":"系统账号（login）","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569575"},{"title":"明道账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569577"},{"title":"企业邮箱账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569579"},{"title":"seafile账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=2031647"},{"title":"jira账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569581"},{"title":"有赞百科账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569583"},{"title":"金蝶账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=5769387"},{"title":"360账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569585"},{"title":"cp账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569585"},{"title":"论坛账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569589"},{"title":"有人app账号","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=7569591"},{"title":"vpn账号&连接指南","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=2031660"},{"title":"wifi连接指南","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=6823323"},{"title":"视频会议使用方法","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=4856871"},{"title":"mac常用操作指南","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=6821031"},{"title":"打印机设置教程","url":"http://doc.qima-inc.com/pages/viewpage.action?pageId=2557164"}]
   }
 }
 
@@ -97,3 +41,6 @@ export default {
     return Promise.resolve(result)
   }
 }
+
+
+
