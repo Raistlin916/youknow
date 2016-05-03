fd412d3e13dcd1149210a58d872358cbc0abde35
gaokai <gaokai@qima-inc.comgit>
2016-05-03 15:34:36 +0800

    fix

--- index.babel.js
+++ index.babel.js
@@ -1,7 +1,15 @@
-'use strict';
+const fs = require('fs');
 
-require("babel-polyfill");
-require("babel-core/register")({
-     presets: ['es2015-node5', 'stage-3']
-});
+const babelrc = fs.readFileSync('./.babelrc');
+let config;
+
+try {
+  config = JSON.parse(babelrc);
+} catch (err) {
+  console.error('==>     ERROR: Error parsing your .babelrc.');
+  console.error(err);
+}
+
+require('babel-core/register')(config);
 require('./app');
+
