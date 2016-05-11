ed71b4b60a7d6587fbd95ec1151a353ce326c85e
gaokai <gaokai@qima-inc.comgit>
2016-05-11 18:18:48 +0800

    improve

--- static/scripts/containers/Intro/index.js
+++ static/scripts/containers/Intro/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { Link } from 'react-router'
 import './intro.scss'
 
-export default () =>
+export default ({ isExternal }) =>
   <div className="intro-wrap">
-    <Link className="intro-enter-link" to="/city">立即开启有赞之旅</Link>
+    {
+      isExternal ?
+        (
+        <div className="intro-enter-link-wrap">
+          <Link className="intro-enter-link" to="/city">wifi连接指南</Link>
+          <Link className="intro-enter-link" to="/city">立即开启有赞之旅</Link>
+        </div>
+        ) : <Link className="intro-enter-link" to="/city">立即开启有赞之旅</Link>
+    }
   </div>
